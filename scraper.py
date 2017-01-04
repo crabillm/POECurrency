c128315122fa202683e36c8287ce069b6d7f8b9b
crabillm <crabillm@purdue.edu>
2017-01-03 21:57:23 -0500

    MONEY MONEY MONEY

--- scraper.py
+++ scraper.py
@@ -5,6 +5,11 @@ rangeend = 9
 matrix = [[0]*(rangeend + 1) for i in range(rangeend + 1)]
 
 
+desiredratio = 1.2
+
+
+
+
 br = webdriver.PhantomJS('phantomjs')
 br.get('http://currency.poe.trade/search?league=Breach&online=x&want=1-2-3-4-5-6-7-8&have=1-2-3-4-5-6-7-8')
 
@@ -16,9 +21,9 @@ for line in sep:
 		tag = line.split('>')[0]
 		data = tag.split('"')[1::2]
 		buytype = int(data[2])
-		buyamt = data[3]
+		buyamt = data[5]
 		selltype = int(data[4])
-		sellamt = data[5]
+		sellamt = data[3]
 		user = data[6]
 		if matrix[buytype][selltype] == 0:
 			matrix[buytype][selltype] = float(float(sellamt)/float(buyamt))
@@ -31,14 +36,14 @@ for i in range(1, rangeend):
 for i in range(1, rangeend):
     for j in range(1, i):
         if i != j:
-            if (matrix[i][j] * matrix[j][i]) > 1.0:
-                print "Cycle of length 2 detected from " + i + " to " + j + " to " + i
+            if (matrix[i][j] * matrix[j][i]) > desiredratio:
+                print "Cycle of length 2 detected from " + str(i) + " to " + str(j) + " to " + str(i)
     
 # Cycles of length 3
 for i in range(1, rangeend):
     for j in range(1, i):
         for k in range(1, j):
             if i != j and j != k and k != i:
-                if (matrix[i][j] * matrix[j][k] * matricx[k][i]) > 1.0:
-                    print "Cycle of length 3 detected from " + i + " to " + j + " to " + k + " to " + i
+                if (matrix[i][j] * matrix[j][k] * matrix[k][i]) > desiredratio:
+                    print "Cycle of length 3 detected from " + str(i) + " to " + str(j) + " to " + str(k) + " to " + str(i)
 
